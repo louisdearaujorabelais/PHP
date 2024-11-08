08bd909987edb4ced011d9eb747d70caf18b288e
unknown <de_araujo@220056slocal>
2024-11-08 16:52:37 +0100

    o

---
+++ formulaire.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>mon 1er php </title>
+    </head>
+    <body>
+    <?php
+        $j = 0;
+        echo "
+        <form action= index.php method=post>
+        Choix d'un nombre pour avoir sa table: <input type=text $j=txtName><br>      
+        <input type=submit>   
+        </form>
+         ";
+    
+    
+    
+    
+    
+    ?>
+    </body>
+</html>
